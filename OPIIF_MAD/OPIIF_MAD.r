efbe0b126703d558c5aab1f1bd4a3bea7d0647b5
IFFranciscoME <if.francisco.me@gmail.com>
2016-03-28 21:16:09 -0600

    Medidas de Atribucion

---
+++ OPIIF_MAD/OPIIF_MAD.r
@@ -0,0 +1,53 @@
+
+# -- -------------------------------------------------- ---------------------------- -- #
+# -- Codigo: PAP Optimizacion de Programas de Inversion ---------------------------- -- #
+# -- Fecha Ult Modificacion: 28.Marzo.2016              ---------------------------- -- #
+# -- GitHub: ------------------------------------------ ---------------------------- -- #
+# -- -------------------------------------------------- ---------------------------- -- #
+
+rm(list=ls())         # Remover objetos del environment
+cat("\014")           # Limpiar la Consola
+
+Pkg <- c("base","fBasics","fPortfolio","grid","httr","lubridate","PerformanceAnalytics",
+         "plyr","quantmod","xts","zoo","quadprog","quantmod","Quandl","ggplot2",
+         "timeDate")
+
+inst <- Pkg %in% installed.packages()
+if(length(Pkg[!inst]) > 0) install.packages(Pkg[!inst])
+instpackages <- lapply(Pkg, library, character.only=TRUE)
+
+# -- ---------------------------------------------------------- Obtencion de precios -- #
+
+FInicial <- "2014-01-01"
+FFinal   <- "2016-02-08"
+
+# -- ------------------------------------------------------------------------------ -- #
+
+# -- Tasa Libre de Riesgo (Cetes) ANUAL
+Rf <- Quandl("BDM/SF282", trim_start=FInicial)
+colnames(Rf) <- c("Index","Rf")
+IndexRf <- Rf$Index
+Rf$Index <- format(Rf$Index, format="%b %Y")
+Rf <- Rf[-1,]
+Rf[,2] <- round((Rf[,2]/25200)*28,4)
+
+# -- BenchMark (IPC)
+Bm <- Cl(get(getSymbols(Symbols="^MXX", env=.GlobalEnv, from=FInicial, to=FFinal)))
+IndexBm <- (fortify.zoo(Bm))[,1]
+Bm <- fortify.zoo(to.monthly(Bm))
+Bm <- data.frame(Bm[-1,1], round(diff(log(Bm[,5])),4))
+colnames(Bm) <- c("Index","Bm")
+  
+# -- Fondo de inversion OldMutual
+Fd <- Cl(get(getSymbols(Symbols="OMRVMXA.MX", env=.GlobalEnv, from=FInicial, to=FFinal)))
+IndexFd <- (fortify.zoo(Fd))[,1]
+Fd <- fortify.zoo(to.monthly(Fd))
+Fd <- data.frame(Fd[-1,1], round(diff(log(Fd[,5])),4))
+colnames(Fd) <- c("Index","Fd")
+
+Df.Datos  <- join_all(list(Rf,Bm,Fd), by = 'Index', type = 'full')
+
+
+Xts.Datos <- as.xts(Df.Datos, order.by = (Df.Datos$Index))
+
+rm(list = "MXX", "OMRVMXA.MX","Bm","Fd","Rf")
